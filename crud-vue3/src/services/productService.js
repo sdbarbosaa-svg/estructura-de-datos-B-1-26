eac51ae9efe1c59092e5cdbc75b8bc093ccdce2f
Shairek Barbosa <sdbarbosaa@ufpso.edu.co>
2026-05-20 19:42:06 -0500

    Actividad

---
+++ crud-vue3/src/services/productService.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+const BASE_URL = 'https://6a0e467f1736097c3609a5f2.mockapi.io'
+const RESOURCE = `${BASE_URL}/products`
+
+export default {
+  getAllProducts() {
+    return axios.get(RESOURCE)
+  },
+
+  getProductById(id) {
+    return axios.get(`${RESOURCE}/${id}`)
+  },
+
+  createProduct(data) {
+    return axios.post(RESOURCE, data)
+  },
+
+  updateProduct(id, data) {
+    return axios.put(`${RESOURCE}/${id}`, data)
+  },
+
+  deleteProduct(id) {
+    return axios.delete(`${RESOURCE}/${id}`)
+  }
+}
